27b845fba98c4fa06d0ba525f8bc982e5b026224
Urfan Guliyev <urfan196@yahoo.com>
2019-10-07 16:59:11 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,12 +4,12 @@ attr_accessor :name, :breed, :age
 
 @@all = []
 
-    def initilaze(name, breed, age)
-      @name = name
-      @breed = breed
-      @age = age
-      @@all << self
-    end
+def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+end
 
     def self.all
       @@all
